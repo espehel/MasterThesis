f219f0c77d131d45ca0aae506b891e1af2063c56
Espen Hellerud <espenhellerud5@gmail.com>
2016-01-30 15:37:37 +0100

    added different options for searching the index

--- Web-Query/elasticsearch/api.js
+++ Web-Query/elasticsearch/api.js
@@ -13,6 +13,7 @@ module.exports.search = function(query_content, callback) {
             query: {
                 match: {
                     "_all": query_content
+
                 }
             }
         }
@@ -22,3 +23,141 @@ module.exports.search = function(query_content, callback) {
     });
 
 };
+
+module.exports.search2 = function(query_content, callback) {
+
+    client.search({
+        index: "wikipedia",
+        body : {
+            "query": {
+                "match": {
+                    "_all": {
+                        "query": query_content,
+                        "minimum_should_match": "75%"
+                    }
+                }
+            }
+        }
+    }, function (error, response) {
+        //console.log(response);
+        callback(error,response.hits.hits);
+    });
+
+};
+
+module.exports.search3 = function(query_content, callback) {
+
+    client.search({
+        index: "wikipedia",
+        body : {
+            "query": {
+                "bool": {
+                    "should": [
+                        { "match": {
+                            "title":  {
+                                "query": query_content,
+                                "boost": 3
+                            }}},
+                        { "match": {
+                            "categories":  {
+                                "query": query_content,
+                                "boost": 2
+                            }}},
+                        { "match": {
+                            "markup":  {
+                                "query": query_content,
+                                "boost": 1
+                            }}},{ "match": {
+                            "introduction":  {
+                                "query": query_content,
+                                "boost": 0.5
+                            }}}
+                    ]
+                }
+            }
+        }
+    }, function (error, response) {
+        //console.log(response);
+        callback(error,response.hits.hits);
+    });
+
+};
+
+module.exports.search4 = function(query_content, callback) {
+
+    client.search({
+        index: "wikipedia",
+        body : {
+            "query": {
+                "dis_max": {
+                    "should": [
+                        { "match": {
+                            "title":  {
+                                "query": query_content,
+                                "boost": 3
+                            }}},
+                        { "match": {
+                            "categories":  {
+                                "query": query_content,
+                                "boost": 2
+                            }}},
+                        { "match": {
+                            "markup":  {
+                                "query": query_content,
+                                "boost": 1
+                            }}},{ "match": {
+                            "introduction":  {
+                                "query": query_content,
+                                "boost": 0.5
+                            }}}
+                    ]
+                }
+            }
+        }
+    }, function (error, response) {
+        //console.log(response);
+        callback(error,response.hits.hits);
+    });
+
+};
+
+module.exports.search5 = function(query_content, callback) {
+
+    client.search({
+        index: "wikipedia",
+        body : {
+            "query": {
+                "dis_max": {
+                    "should": [
+                        { "match": {
+                            "title":  {
+                                "query": query_content,
+                                "boost": 3
+                            }}},
+                        { "match": {
+                            "categories":  {
+                                "query": query_content,
+                                "boost": 2
+                            }}},
+                        { "match": {
+                            "markup":  {
+                                "query": query_content,
+                                "boost": 1
+                            }}},{ "match": {
+                            "introduction":  {
+                                "query": query_content,
+                                "boost": 0.5
+                            }}}
+                    ],
+                    "tie_breaker": 0.3
+                }
+            }
+        }
+    }, function (error, response) {
+        //console.log(response);
+        callback(error,response.hits.hits);
+    });
+
+};
+
+
